33ee2836eac4f8fe2588d764d3c1f013211ef083
Abdullah Mahmudov <abdulla.mahmudov@nspsolutions.net>
2022-11-20 02:13:29 +0400

    game enhancements added stage-5

--- Words Virtuoso/task/src/wordsvirtuoso/Main.kt
+++ Words Virtuoso/task/src/wordsvirtuoso/Main.kt
@@ -1,7 +1,9 @@
 package wordsvirtuoso
 
 import java.io.File
+import java.lang.System.currentTimeMillis
 import java.util.*
+import kotlin.properties.Delegates
 
 val scanner = Scanner(System.`in`)
 val generator = Random()
@@ -18,39 +20,56 @@ class WordsVirtuoso {
                 var invalids = 0
                 allWords.forEach { if (!it.checkWord()) invalids++ }
                 if (invalids == 0) {
-                    invalids = 0
                     val candidateWords = candidateWordsFile.readLines().map { it.lowercase() }
+                    invalids = 0
                     candidateWords.forEach { if (!it.checkWord()) invalids++ }
                     if (invalids == 0) {
                         var notContained = 0
                         candidateWords.forEach { if (!allWords.contains(it)) notContained++ }
                         if (notContained == 0) {
-                            println("Words Virtuoso")
-                            println()
+                            println("Words Virtuoso\n")
                             val secretIndex = generator.nextInt(candidateWords.size)
                             val secretWord = candidateWords[secretIndex]
+                            var tries = 0
+                            var wrongChars = setOf<Char>()
+                            var startTime by Delegates.notNull<Long>()
+                            var output = ""
                             game@ while (true) {
                                 val word = getWord()
-                                if (word != null)  {
+                                if (tries == 0) startTime = currentTimeMillis()
+                                if (word != null) {
                                     if (word == "exit") {
                                         println("The game is over.")
                                         break@game
                                     }
+                                    tries++
                                     if (word == secretWord) {
-                                        println("Correct!")
+                                        if (tries == 1) println("${secretWord.uppercase()}\n\nCorrect!\nAmazing luck! The solution was found at once.")
+                                        else {
+                                            val duration =
+                                                ((currentTimeMillis() - startTime).toDouble() / 1000.0).toLong()
+                                            println(output)
+                                            println("${secretWord.uppercase()}\n\nCorrect!\nThe solution was found after $tries tries in $duration seconds.")
+                                        }
                                         break@game
                                     }
                                     if (!allWords.contains(word)) {
                                         println("The input word isn't included in my words list.")
                                         continue@game
                                     }
-                                    var output = ""
                                     word.forEach {
-                                        output += if (secretWord.contains(it))
-                                            if (secretWord.indexOf(it) == word.indexOf(it)) it.uppercase() else it.lowercase()
-                                        else '_'
+                                        output += if (secretWord.contains(it)) {
+                                            if (secretWord.indexOf(it) == word.indexOf(it)) {
+                                                it.uppercase()
+                                            } else it.lowercase()
+                                        } else {
+                                            wrongChars = wrongChars.plus(it.uppercaseChar())
+                                            '_'
+                                        }
                                     }
                                     println(output)
+                                    println(wrongChars.sorted().joinToString(""))
+                                    output += '\n'
                                 }
                             }
                         } else println("Error: $notContained candidate words are not included in the $allWordsPath file.")
